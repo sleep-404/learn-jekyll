fd593a3027490e59b71bce48aa310f2589fef0f7
sleep-404 <justjeevan07@gmail.com>
2018-05-29 11:03:48 +0530

    messed up side-nav completly

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 console.log('1');
 var count =0;
+
 const press = document.getElementById('drop-down');
 const drop = document.getElementById('side-content');
 var change = function (){
@@ -12,4 +13,58 @@ var change = function (){
     drop.style.display = 'none';
     count++;
   }
+}
+
+var sidenav = document.getElementById('side-content');
+var myElement = document.querySelector("#workover");
+var position = getPosition(myElement);
+
+var count1=0;
+
+function getPosition(el) {
+  var xPos = 0;
+  var yPos = 0;
+  var heit = screen.height;
+
+
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+
+    el = el.offsetParent;
+  }
+
+  console.log(yPos+' ypos');
+  console.log(heit+' heit');
+  if(yPos<=heit){
+    sidenav.className = 'after';
+    myElement.style.visibility = 'hidden';
+    console.log(yPos+':yPos and heit:'+heit+'done');
+  }
+  else {
+    sidenav.className = 'before';
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
+
+}
+
+// deal with the page getting resized or scrolled
+window.addEventListener("scroll", updatePosition, false);
+window.addEventListener("resize", updatePosition, false);
+
+function updatePosition() {
+  position = getPosition(myElement);
 }

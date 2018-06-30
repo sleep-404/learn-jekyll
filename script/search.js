08861f997acda32c450536e5ce2c63904ed8985c
sleep-404 <justjeevan07@gmail.com>
2018-06-30 19:32:06 +0530

    Hurray search is now working fine !

--- script/search.js
+++ script/search.js
@@ -7,14 +7,9 @@ for(i=0;i<posts.length;i++){
   tags.push(posts[i].childNodes[3]);
 }
 // console.log(tags[0].children[0].innerHTML.length);
-<<<<<<< HEAD
 // console.log(window.location.search.split('?query=')[1]);
-// var query = window.location.search.split('?query=')[1];
-var query = 'as'
-=======
-
-var query = 's';
->>>>>>> parent of 5ae81a1... F*ck, the search isn't working :(
+var query = window.location.search.split('?query=')[1];
+// var query = 'as'
 var patt = new RegExp(query,'gim');
 
 
@@ -25,49 +20,67 @@ for(k=0;k<posts.length;k++){
   var meta = posts[k].childNodes[1].children[1].innerHTML;
   if(patt.test(title)){
     score+=5;
+    // console.log(title+' title');
   }
   if(patt.test(meta)){
     score+=4;
+    // console.log(meta+' meta');
   }
   i=0;
-  var mid = search(query,i,j) ;
+  var mid = search(query,i,j,k) ;
   var first,last;
   if(mid!=-1){
     // console.log(mid+' mid ');
     // console.log(tags[k].children[mid].innerHTML)
-    first = range(mid,query[0])[0];
-    last = range(mid,query[0])[1];
+    first = range(mid,query[0],k,j)[0];
+    last = range(mid,query[0],k,j)[1];
     // console.log(first+' :first ; last: '+last);
     var title;
     for(i=first;i<last;i=i+2){
       if(patt.test(tags[k].children[i].innerHTML)){
+        // console.log(tags[k].children[i].innerHTML+' is the inner HTML');
         score=score+Number(tags[k].children[i+1].innerHTML);
       }
     }
   }
-  // console.log(score+' I am the score');
+  console.log(score+' I am the score' + ' ; I am K :' + k);
   if(score){
     var key = new Key_value(k,score);
     analized.push(key);
-    console.log("Yoyo pandua");
+    posts[k].score = score;
+    console.log(posts[k].score+' This has been added ');
+  }
+  else {
+    posts[k].score = 0;
   }
 }
 //ended
 var result  = document.getElementById('content');
+
+
+
+// console.log(typeof (analized));
+// console.log(typeof (posts) );
+// console.log(posts);
+// console.log(analized);
+
 if(analized.length){
   analized.sort(function compare(a,b){
+    // console.log(typeof(b.score));
     return b.score - a.score;
   });
-  console.log(posts[0]);
+console.log(result);
   for(i=0;i<analized.length;i++){
-    result.children[i] = posts[analized[i].index];
+    result.children[i].innerHTML = posts[analized[i].index].innerHTML;
+    // console.log(posts[analized[i].index]);
+    // console.log(result.children[i]);
   }
-    console.log(posts[0]);
+// console.log(result);
+
+    // console.log(posts[0]);
   for(i=analized.length;i<posts.length;i++){
       result.children[i].style.display='none';
   }
-  // console.log(posts_fix[0]);
-  // result.innerHTML += posts_fix[0];
 }
 
 else {
@@ -78,29 +91,31 @@ else {
 
 
 
-function range(mid,query_1){
+function range(mid,query_1,k){
   var x=mid,y=mid;
-  while(tags[0].children[x].innerHTML[0]==query_1){
+  while( x>=0 && tags[k].children[x].innerHTML[0]==query_1 ){
     x=x-2;
   }
-  while(tags[0].children[y].innerHTML[0]==query_1){
+  while(y<=j && tags[k].children[y].innerHTML[0]==query_1){
     y=y+2;
   }
   x=x+2,y=y;
+  // console.log(tags[k].children[x].innerHTML,tags[k].children[y-2].innerHTML);
   return [x,y];
 }
 
-function search(query,i,j) {
+function search(query,i,j,k) {
   var  mid = ( (i+j) - ( (i+j)%2 ) ) /2;
   if(mid%2){
     mid=mid-1;
   }
   while(i<=j){
     // console.log( i + ' = low ;' + mid + ' = mid ;'  + j + ' = high;');
-    if(tags[0].children[mid].innerHTML[0].localeCompare(query[0])==0){
+    if(tags[k].children[mid].innerHTML[0].localeCompare(query[0])==0){
+      // console.log(tags[k].children[mid].innerHTML + ' From search and k is : ' + k);
       return mid;
     }
-    else if(tags[0].children[mid].innerHTML[0].localeCompare(query[0])==1){
+    else if(tags[k].children[mid].innerHTML[0].localeCompare(query[0])==1){
       j = mid-2;
     }
     else {
